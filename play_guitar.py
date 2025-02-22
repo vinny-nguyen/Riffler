acc63e10b37b80db56645d6278364e5c778e7b11
Haokai Xuan <haokai.xuan2006@gmail.com>
2025-02-22 10:54:02 -0500

    play guitar prototype

--- play_guitar.py
+++ play_guitar.py
@@ -1,6 +1,11 @@
 import json
 import time
 
+def precise_sleep(duration_ms):
+    start = time.perf_counter_ns()
+    while (time.perf_counter_ns() - start < (duration_ms * 1e6)):
+        pass
+
 with open("guitar_events.json", "r") as file:
     events = json.load(file)
 
@@ -8,16 +13,14 @@ timestamp = 0 # In milliseconds
 
 # Simulate playing process
 event_index = 0
+total_events = len(events)
 
-for event in events:
-    string = event["string"]
-    fret = event["fret"]
-    start = int(event["start"])
-    
-    if timestamp == start:
-        # Do the required cmd
-        while :
-    
-    time.sleep(0.01) # Sleep for 10ms
+while event_index < total_events:
+    # For all events with same timestamp
+    while event_index < total_events and events[event_index]["start"] == timestamp:
+        event = events[event_index]
+        print(f"At {event["start"]}ms: Play string {event["string"]} at fret {event["fret"]}")
+        event_index += 1
+
     timestamp += 10
-
+    precise_sleep(10)
